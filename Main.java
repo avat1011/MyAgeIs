dfaeabcfc9db46a461c4281bd6a7b93c045c68a9
Ava Tran <ava.tran@patelhs.org>
2020-09-22 14:02:48 +0000

    I declared and initialized my ints for myAge. myBirthday, and todaysDate.  m

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+class Main{
+  public static void main(String[] args){
+    int myAge = 15;
+    int myBirthday = 1011;
+    int todaysDate = 921; 
+
+    if(myBirthday == todaysDate)
+    {
+      myAge = myAge + 1; 
+      System.out.print("I am " + myAge + "years old");
+    } else {
+      System.out.print("I am still " + myAge + " years old");
+    }
+
+
+    
+   
   }
 }
